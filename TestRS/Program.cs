3a8a311ddff4377b7a1c206c7f2202e947df06ae
Sandro Magi <none@none>
2025-02-14 11:27:53 -0500

    initial import

---
+++ TestRS/Program.cs
@@ -0,0 +1,36 @@
+using RazorSlices;
+
+namespace TestRS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            app.MapGet("/", (HttpContext httpContext) =>
+            {
+                return Results.Extensions.RazorSlice<TestRS.Slices.Test>();
+            });
+
+            app.Run();
+        }
+    }
+}
